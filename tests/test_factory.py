7ec8dd5c238edebda5802ea1cfbf67a61ee988c4
Shikha Singh <siya@Shikhas-MacBook-Pro.local>
2026-01-16 01:57:42 +0530

    adds test for initial factory creation and logger creation

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,2 +1,10 @@
+from logger.console_logger import ConsoleLoggerFactory, ConsoleLogger
+
+def test_factory_creates_console_logger():
+    factory = ConsoleLoggerFactory()
+    logger = factory.create()
+    assert isinstance(factory, ConsoleLoggerFactory)
+    assert isinstance(logger, ConsoleLogger)
+
 def test_dummy():
     assert True
