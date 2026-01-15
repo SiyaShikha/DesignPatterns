834477eafd30e1eb6c549598abfdee14e4b95499
Shikha Singh <siya@Shikhas-MacBook-Pro.local>
2026-01-16 02:09:54 +0530

    adds test for abstract types

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,10 +1,19 @@
+from logger.logger import Logger, LoggerFactory
 from logger.console_logger import ConsoleLoggerFactory, ConsoleLogger
 
 def test_factory_creates_console_logger():
     factory = ConsoleLoggerFactory()
     logger = factory.create()
+
     assert isinstance(factory, ConsoleLoggerFactory)
     assert isinstance(logger, ConsoleLogger)
+
+def test_factory_abstract_types():
+    factory = ConsoleLoggerFactory()
+    logger = factory.create()
+
+    assert isinstance(factory, LoggerFactory)
+    assert isinstance(logger, Logger)
 
 def test_dummy():
     assert True
