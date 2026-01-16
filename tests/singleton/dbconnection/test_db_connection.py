1ee82881c8ea971cf90a814f286b8736990dfead
Shikha Singh <siya@Shikhas-MacBook-Pro.local>
2026-01-16 16:56:59 +0530

    adds directory dbconnection

--- tests/singleton/dbconnection/test_db_connection.py
+++ tests/singleton/dbconnection/test_db_connection.py
@@ -1,4 +1,4 @@
-from singleton.db_connection import DatabaseConnection
+from singleton.dbconnection.db_connection import DatabaseConnection
 
 def test_returns_same_instance():
     db1 = DatabaseConnection.get_instance()
