dca85bc42c8ad0e031a4f233ad4e5d1c931d29fc
Shikha Singh <siya@Shikhas-MacBook-Pro.local>
2026-01-19 11:21:52 +0530

    adds test for notification send methods

--- tests/factory/notification/test_email_notificaiton.py
+++ tests/factory/notification/test_email_notificaiton.py
@@ -4,3 +4,10 @@ from factory.notification.email_notification import EmailNotification
 def test_email_notification_type():
     notification = EmailNotification()
     assert isinstance(notification, EmailNotification)
+
+def test_email_notification_send(capfd):
+    notification = EmailNotification()
+    test_message = "Hello via Email"
+    notification.send(test_message)
+    captured = capfd.readouterr()
+    assert captured.out == f"Sending Email: {test_message}\n"
--- tests/factory/notification/test_sms_notification.py
+++ tests/factory/notification/test_sms_notification.py
@@ -3,3 +3,11 @@ from factory.notification.sms_notification import SMSNotification
 def test_sms_notification_type():
     notification = SMSNotification()
     assert isinstance(notification, SMSNotification)
+
+def test_sms_notification_send(capfd):
+    notification = SMSNotification()
+    test_message = "Hello via SMS"
+    notification.send(test_message)
+    captured = capfd.readouterr()
+    print("captured text",captured)
+    assert captured.out == f"Sending SMS: {test_message}\n"
