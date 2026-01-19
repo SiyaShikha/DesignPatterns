c8534617e687d5ad1574944fc90abaa00d7f36f4
Shikha Singh <siya@Shikhas-MacBook-Pro.local>
2026-01-19 10:45:01 +0530

    renames test method

--- tests/factory/logger/test_interface.py
+++ tests/factory/logger/test_interface.py
@@ -2,11 +2,11 @@ from factory.logger.interface import Logger, LoggerFactory
 from factory.logger.console_logger import ConsoleLoggerFactory
 
 
-def test_logger_abstract_types():
+def test_logger_abstract_type():
     factory = ConsoleLoggerFactory()
     logger = factory.create_logger()
     assert isinstance(logger, Logger)
 
-def test_logger_factory_abstract_types():
+def test_logger_factory_abstract_type():
     factory = ConsoleLoggerFactory()
     assert isinstance(factory, LoggerFactory)
