f45e78e5429e67642dbc8625652ce88bbf5b8294
Shikha Singh <siya@Shikhas-MacBook-Pro.local>
2026-01-29 18:06:20 +0530

    fixes test case for sorter

--- src/strategy/sort.py
+++ src/strategy/sort.py
@@ -1,4 +1,4 @@
-from .sorting_algo import BubbleSort
+from strategy.sorting_algo import BubbleSort
 
 class Sort:
     def __init__(self, algo):
@@ -6,11 +6,7 @@ class Sort:
     def sort(self,arr):
         return self._algo.perform_sort(arr)
 
-bubble_sorter = BubbleSort()
-arr = [9,0,7,8,70,6,98]
-sorter = Sort(bubble_sorter)
-print(sorter.sort(arr))
-
-
-        
-        
+# bubble_sorter = BubbleSort()
+# arr = [9,0,7,8,70,6,98]
+# sorter = Sort(bubble_sorter)
+# print(sorter.sort(arr))
--- tests/strategy/test_sorting.py
+++ tests/strategy/test_sorting.py
@@ -1,10 +1,10 @@
-from DesignPatterns.src.strategy.sort import Sort
-from DesignPatterns.src.strategy.sorting_algo import BubbleSort
+from strategy.sort import Sort
+from strategy.sorting_algo import BubbleSort
 
 
 def test_sort():
     a = [1,5,6,78,4]
-    bubble_sort = BubbleSort()
-    sorted_list = Sort(bubble_sort , a)
-    assert sorted_list == [1,4,5,6,78]
+    bubble_sorter = BubbleSort()
+    sorter = Sort(bubble_sorter)
+    assert sorter.sort(a) == [1,4,5,6,78]
     
