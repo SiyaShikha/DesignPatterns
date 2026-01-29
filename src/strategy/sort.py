3bdf77a3c820d091cc24bb05e0cd74977e29d36a
KhushiMittal9876 <kmittal4957@gmail.com>
2026-01-29 17:59:51 +0530

    implements strategy pattern for sorting

---
+++ src/strategy/sort.py
@@ -0,0 +1,16 @@
+from .sorting_algo import BubbleSort
+
+class Sort:
+    def __init__(self, algo):
+        self._algo = algo
+    def sort(self,arr):
+        return self._algo.perform_sort(arr)
+
+bubble_sorter = BubbleSort()
+arr = [9,0,7,8,70,6,98]
+sorter = Sort(bubble_sorter)
+print(sorter.sort(arr))
+
+
+        
+        
---
+++ src/strategy/sorting_algo.py
@@ -0,0 +1,26 @@
+from abc import ABC,abstractmethod
+
+class SortingAlgo(ABC):
+    @abstractmethod
+    def perform_sort(self,arr):
+        pass    
+    
+class BubbleSort(SortingAlgo):
+    def perform_sort(self, arr):
+        n = len(arr)
+        for i in range(n):
+            swapped = False
+            for j in range(0, n - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                    swapped = True
+            if not swapped:
+                break 
+        return arr 
+    
+# class SelectionSort:
+# class InsertionSort:
+# class MergeSort:
+# class QuickSort:
+    
+    
